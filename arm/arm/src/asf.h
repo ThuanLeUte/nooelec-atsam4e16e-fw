fbc6907567c1c4d0a0bcb0ae7c61be477f2b6a2c
Thuan Le <devicedev4@biotricity.com>
2021-10-09 00:16:51 +0700

    update: add RTOS

--- arm/arm/src/asf.h
+++ arm/arm/src/asf.h
@@ -68,6 +68,24 @@
 #include <ffconf.h>
 #include <integer.h>
 
+// From module: FreeRTOS - kernel 10.0.0
+#include <FreeRTOS.h>
+#include <StackMacros.h>
+#include <croutine.h>
+#include <deprecated_definitions.h>
+#include <event_groups.h>
+#include <list.h>
+#include <message_buffer.h>
+#include <mpu_wrappers.h>
+#include <portable.h>
+#include <projdefs.h>
+#include <queue.h>
+#include <semphr.h>
+#include <stack_macros.h>
+#include <stream_buffer.h>
+#include <task.h>
+#include <timers.h>
+
 // From module: GPIO - General purpose Input/Output
 #include <gpio.h>
 
